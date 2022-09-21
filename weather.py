f7e65896e418f0a00f93323387c2a8d392fecac0
Andrew <70990567+apakula036@users.noreply.github.com>
2022-09-21 15:42:01 -0700

    Finished the Project
    Using the weather API a user can now type the city and it will give the weather back for that city.

--- weather.py
+++ weather.py
@@ -1,7 +1,24 @@
+from locale import format_string
+from urllib import request, response
 import requests
 import config
 
-API_KEY = config.API_KEY
+API_KEY1 = config.API_KEY
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
-print(API_KEY)
+city = input("Enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY1}&q={city}&units=imperial"
+
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    weather = data["weather"][0]["description"]
+    temperature = round(data["main"]["temp"])
+
+    print("The weather is:", weather + ".")
+    print("It is currently" , temperature , "degrees farenheit.")
+
+    # print(data)
+else:
+    print("An error occurred.")
